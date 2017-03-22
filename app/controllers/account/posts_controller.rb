d76450082c51ae90d94b87e6541c199fbe312626
Duang-JiangFan <duang@duagn.sh>
2017-03-22 20:44:25 +0800

    use simple_format helper to decorate, use self delimit helper

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,15 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+  def edit
+      @post = Post.find(params[:id])
+    @post.group = @group
+  end
+  def destory
+    redirect_to groups_path, alert:'Post deleted'
+  end
+  def post_params
+    params.require(:post.group,:post).permit(:content)
+  end
+
 end
